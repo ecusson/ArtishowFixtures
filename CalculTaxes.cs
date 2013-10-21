6628fd1369f6629877be66d9dcc3b3c2cebe30af
mathieu bergeron <mabehec@Macintosh-2.local>
2013-10-21 16:12:14 -0400

    asdfasdf
    asdfas

---
+++ CalculTaxes.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace artishowFixture
+{
+	public class CalculTaxes
+	{
+		public CalculTaxes (int TpsTaux, int TvqTaux)
+		{
+		}
+
+		public int calculTaxes(int montantNet)
+		{
+			return 0;
+
+		}
+	}
+}
+
---
+++ GénérerDesRéservations.cs
@@ -0,0 +1,41 @@
+using System;
+
+namespace artishowFixture
+{
+	public class GénérerDesRéservations
+	{
+		public GénérerDesRéservations ()
+		{
+		}
+
+		public void CréerRéservation(string réservation)
+		{
+
+		}
+
+		public void AjouterReprésentation(string spectacle)
+		{
+
+		}
+
+		public void AjouterClient(string client)
+		{
+
+		}
+
+		public void AjouterSiegeAuPrixDe(string siege, string prix)
+		{
+
+		}
+		public void AjouterFraisAuMontantDe(string nomFrais, string prix)
+		{
+
+		}
+		public void AjouterItemAuPrixDe(string item, string prix)
+		{
+
+		}
+
+	}
+}
+
