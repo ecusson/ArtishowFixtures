64268089c5c34478d77aa49bac63892b9d88d567
mabehec <mabehec@Macintosh-2.local>
2013-10-24 20:16:00 -0400

    nouveau tests frais de service

---
+++ AjouterFraisServiceSurReservation.cs
@@ -0,0 +1,32 @@
+using System;
+
+namespace artishowFixture
+{
+	public class AjouterFraisServiceSurReservation
+	{
+		public AjouterFraisServiceSurReservation ()
+		{
+		}
+		public void CreerReservationAuNomDeAvecTotalDe(string noReservation, string nomClient, decimal total)
+		{
+
+		}
+
+		public void AjouterFraisSurReservation(decimal frais, string noReservation)
+		{
+
+		}
+
+		public decimal TotalReservation(string noReservation)
+		{
+			return 100m;
+		}
+
+
+
+
+
+
+	}
+}
+
---
+++ AjouterFraisServiceSurTransaction.cs
@@ -0,0 +1,27 @@
+using System;
+
+namespace artishowFixture
+{
+	public class AjouterFraisServiceSurTransaction
+	{
+		public AjouterFraisServiceSurTransaction ()
+		{
+		}
+
+		public void CreerReservationAuNomDeAvecTotalDe(string noReservation, string nomClient, decimal total)
+		{
+
+		}
+
+		public void AjouterFraisSurTransaction(decimal frais, string noTransaction)
+		{
+
+		}
+
+		public decimal TotalTransaction(string noTransaction)
+		{
+			return 255m;
+		}
+	}
+}
+
