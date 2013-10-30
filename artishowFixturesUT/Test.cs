72dad3938c11faaae6bdae214828134e529b24f9
ecusson <ecusson@gmail.com>
2013-10-29 23:44:36 -0400

    ajouter test unitaire pour démontrer fixture

--- artishowFixturesUT/Test.cs
+++ artishowFixturesUT/Test.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using System;
+using artishowFixture;
 
 namespace artishowFixturesUT
 {
@@ -9,6 +10,18 @@ namespace artishowFixturesUT
 		[Test ()]
 		public void TestCase ()
 		{
+
+			var test = new AjouterPlusieursSiegesDansReservation ();
+			test.GenererInventaire ("A1");
+			test.GenererInventaire ("A2");
+			test.GenererInventaire ("A3");
+			test.ReserverBilletPourClient ("A1", "CLIENT1");
+			test.ReserverBilletPourClient ("A2", "CLIENT1");
+			Assert.IsTrue(test.inventaireContientPas ("A1"));
+			Assert.IsFalse(test.inventaireContientPas ("A3"));
+			test.ReserverBilletPourClient ("A3", "CLIENT1");
+			Assert.IsTrue(test.inventaireContientPas ("A3"));
+
 		}
 	}
 }
